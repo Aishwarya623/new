823c6a2d9a501c448c045a06b2d7a087e244f0eb
Aishwarya623 <paishwarya623.com>
2025-03-18 16:13:37 +0530

    Normalize line endings

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+import postcssConfig from './postcss.config.cjs';
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// vite.config.ts
+
 
 export default defineConfig({
   plugins: [react()],
